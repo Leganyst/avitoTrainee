aa36e0e54412b0d5285d86ec6e166dcbd3a1754c
Марк <Leganyst@yandex.ru>
2025-11-19 22:21:11 +0300

    chore: дополнить изменения к предыдущему коммиту

--- internal/service/pr.go
+++ internal/service/pr.go
@@ -1,1 +1,14 @@
 package service
+
+import "github.com/Leganyst/avitoTrainee/internal/model"
+
+type PRRepository interface {
+	CreatePR(pr *model.PullRequest) error
+	GetPRByExternalID(prID string) (*model.PullRequest, error)
+	UpdatePR(prID string) error
+
+	AddReviewers(pr *model.PullRequest, reviewers []model.User) error
+	ReplaceReviewer(pr *model.PullRequest, oldReviewerID, newReviewerID uint) error
+
+	GetPRsWhereReviewer(userID uint) ([]model.PullRequest, error)
+}
