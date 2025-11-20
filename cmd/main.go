d1d277bf0da817f91a61fa2845cb799e0562a3f3
Марк <Leganyst@yandex.ru>
2025-11-21 01:11:25 +0300

    feat: Добавить генерацию документации Swagger (openAPI)

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	docs "github.com/Leganyst/avitoTrainee/docs"
 	"github.com/Leganyst/avitoTrainee/internal/config"
 	"github.com/Leganyst/avitoTrainee/internal/controller/handlers"
 	"github.com/Leganyst/avitoTrainee/internal/db"
 	"github.com/Leganyst/avitoTrainee/internal/repository"
 	"github.com/Leganyst/avitoTrainee/internal/service"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           PR Reviewer Service API
+// @version         1.0
+// @description     Service for assigning reviewers to pull requests.
+// @BasePath        /
 func main() {
 	cfg := config.Load()
 	log.Println("config loaded")
+
+	docs.SwaggerInfo.BasePath = "/"
 
 	conn, err := db.Connect(cfg)
 	if err != nil {
@@ -32,6 +41,8 @@ func main() {
 	r := gin.Default()
 
 	handlers.RegisterRoutes(r, teamSvc, userSvc, prSvc)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	srv := &http.Server{
 		Addr:    ":8080",
